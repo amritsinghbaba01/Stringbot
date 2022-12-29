7b018d2d055628cb0fe374b2c77103ddbbdf2274
amritsinghbaba01 <107133886+amritsinghbaba01@users.noreply.github.com>
2022-12-29 13:44:06 +0530

    Update env.py

--- env.py
+++ env.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
+API_ID = os.getenv("API_ID", "13673662").strip()
+API_HASH = os.getenv("API_HASH", "90a8ca652259ec84f9ab4afdc7e3846e").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split()))
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "5144413993").split()))
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://amritsinghbaba01:1998@cluster0.3tacgfj.mongodb.net/?retryWrites=true&w=majority").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "arbackup1")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
